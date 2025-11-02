e650da0387a73001f6f88e210093dcead6f1e093
abdallahshaalan555-cloud <abdallahshaalan555@gmail.com>
2025-11-02 22:31:22 +0200

    Update database_index.sql

--- database-adv-script/file3/database_index.sql
+++ database-adv-script/file3/database_index.sql
@@ -1,1 +1,21 @@
+-- Create index on users.email to speed up user lookups
+CREATE INDEX idx_users_email ON users(email);
+
+-- Create index on bookings.user_id to improve join performance between users and bookings
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+
+-- Create index on bookings.property_id to speed up joins between bookings and properties
+CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+
+-- Create index on properties.city to improve search by location
+CREATE INDEX idx_properties_city ON properties(city);
+
+-- Create index on properties.price to optimize ordering and filtering by price
+CREATE INDEX idx_properties_price ON properties(price);
+
+
+-- Measure performance before and after creating indexes
+-- Example:
+-- EXPLAIN SELECT * FROM bookings WHERE user_id = 5;
+-- EXPLAIN ANALYZE SELECT * FROM properties WHERE city = 'Cairo';
 
