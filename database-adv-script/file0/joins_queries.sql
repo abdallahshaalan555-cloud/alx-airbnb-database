9816dccad97e661aac19d58448766561a24b2412
abdallahshaalan555-cloud <abdallahshaalan555@gmail.com>
2025-11-02 22:19:43 +0200

    Update joins_queries.sql

--- database-adv-script/file0/joins_queries.sql
+++ database-adv-script/file0/joins_queries.sql
@@ -1,1 +1,47 @@
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    bookings.id AS booking_id,
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+INNER JOIN users
+ON bookings.user_id = users.id;
+
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.comment,
+    reviews.rating
+FROM properties
+LEFT JOIN reviews
+ON properties.id = reviews.property_id;
+
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+-- Note: MySQL doesn't support FULL OUTER JOIN directly, so we use UNION between LEFT and RIGHT joins
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id
+FROM users
+LEFT JOIN bookings
+ON users.id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id
+FROM users
+RIGHT JOIN bookings
+ON users.id = bookings.user_id;
 
